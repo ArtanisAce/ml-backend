483d95e11a769fa9b8c6d6772b754f91e721b458
Pedro Pablo Miron Pozo <ppmironpozo@gmail.com>
2018-03-21 03:06:36 +0100

    added cors

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
