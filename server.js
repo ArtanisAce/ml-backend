d14ad9b416c0138dd270394e3dbfe0ff171799c0
Pedro Pablo Miron Pozo <ppmironpozo@gmail.com>
2018-03-21 03:02:17 +0100

    eliminated view engine and refactor code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('hola');
+});
+
+module.exports = app;
